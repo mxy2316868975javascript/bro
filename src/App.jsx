6f7e28601f2e800b2caae2bb27865766bd7710c6
LeafYeeXYZ <leafyeexyz@outlook.com>
2024-04-04 18:49:18 +0800

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
+import { useEffect } from 'react'
 import './App.css'
 import cover from './img/cover.jpg'
 import Header from './header/header.jsx'
 import Footer from './footer/footer.jsx'
 import Section from './section/section.jsx'
 import { IntroTyped } from './typed/typed.jsx'
+import { denoCountURL } from './config.js'
 
 function App() {
+  // 加载 DenoCount
+  useEffect(() => {
+    // 创建 script 标签
+    const script = document.createElement('script')
+    // 设置属性
+    script.src = `${denoCountURL}/static`
+    script.async = true
+    script.defer = true
+    script.dataset.db = denoCountURL
+    // 添加到 body
+    document.body.appendChild(script)
+    // 卸载
+    return () => {
+      document.body.removeChild(script)
+    }
+  }, [])
+  // 渲染
   return (
     <main>
-
       <Header cover={cover} />
-
       <Section direction='row' title='我的项目( ╯▽╰)' subtitle='这里是小叶子的个人小站~'>
         <div style={{
           display: 'flex',
@@ -26,20 +43,15 @@ function App() {
           <p><a href='https://github.com/LeafYeeXYZ'>我的 Github ↗</a></p>
           <p><a href='https://legacy.leafyee.xyz'>旧版网站 ↗</a></p>
           <p>本站总访问量: <span id='SV'></span> (自2024.1.1)</p>
-          <script src="https://denocount.deno.dev/static" data-db="https://count.leafyee.xyz"></script>
         </div>
       </Section>
-
       <Section direction='row-reverse' title='关于我|ω・）' subtitle='小叶子的自我介绍!'>
         <IntroTyped />
       </Section>
-
       <Section direction='row' title='留言板(๑￫ܫ￩)' subtitle='可以匿名留言哦~'>
         <iframe src='/comment.html' width='100%' height='100%' frameBorder='' id='waline'></iframe>
       </Section>
-
       <Footer />
-
     </main>
   )
 }
--- src/config.js
+++ src/config.js
@@ -2,6 +2,7 @@
  * @fileoverview 集中设置一些内容
  */
 
+// 个人简介的 Typed.js 配置
 const introTypedConfig = {
   strings: ['一个心理学专业的大学生<br>一个社会自由主义者<br>喜欢漫画 / 动漫 / BJD<br>喜欢水彩 / 板绘 / 设计<br>喜欢草莓 / 鱿鱼 / 抹茶 / 无骨鸡爪<br>对网络技术 / 编程感兴趣<br>主要使用 JavaScript, 自学 Rust 中<br>喜欢可爱的人事物<br>致力于成为优秀的心理咨询师'],
   typeSpeed: 70,
@@ -12,7 +13,7 @@ const introTypedConfig = {
   showCursor: false,
   startDelay: 2000,
 }
-
+// 标题的 Typed.js 配置
 const titleTypedConfig = {
   strings: ['莫听穿林打叶声，何妨吟啸且徐行', '知善知恶是良知，为善去恶是格物'],
   typeSpeed: 70,
@@ -24,5 +25,7 @@ const titleTypedConfig = {
   contentType: 'html',
   showCursor: false,
 }
+// DenoCount 的 URL
+const denoCountURL = 'https://count.leafyee.xyz'
 
-export { introTypedConfig, titleTypedConfig }
+export { introTypedConfig, titleTypedConfig, denoCountURL }
