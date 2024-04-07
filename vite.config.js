f0a620f82eee0c2854217c491825b636259a7601
LeafYeeXYZ <leafyeexyz@outlook.com>
2024-04-07 11:33:20 +0800

    单独打包antd,优化性能

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 600
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          antd: ['antd'],
+        }
+      }
+    }
   }
 })
